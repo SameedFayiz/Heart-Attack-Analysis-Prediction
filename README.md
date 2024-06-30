b7f4476c2d2e7b8e9aa73dbf98775e76e822a408
sameed fayiz <sameedfayiz@gmail.com>
2024-06-30 17:05:55 +0500

    updated the main page

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ This repository contains a project focused on analyzing and predicting heart att
 
 ## Get Started
 1. **Install Requirements:** Make sure you have all the necessary libraries installed. You can do this by running ```pip install -r requirements.txt```.
-2. **Launch the Application:** Start the Streamlit app by running ```streamlit run index.py``` in your terminal.\n
+2. **Launch the Application:** Start the Streamlit app by running ```streamlit run index.py``` in your terminal.
 3. **Explore and Predict:** Dive into the data, train models, and make predictions!
+
+## Explore the Dataset
+Visit: https://www.kaggle.com/datasets/rashikrahmanpritom/heart-attack-analysis-prediction-dataset
--- index.py
+++ index.py
@@ -8,4 +8,42 @@ viewSideBar()
 
 # ----------------------------------------------
 st.title("Heart attack Analysis & Prediction")
-st.write("This repository contains a project focused on analyzing and predicting heart attacks using machine learning techniques. The application is built with Streamlit, providing an interactive interface for users to explore and predict heart.")
+st.write("Welcome to the Heart Attack Analysis & Prediction application! This project aims to provide a comprehensive tool for analyzing and predicting heart attacks using state-of-the-art machine learning techniques. Built with Streamlit, our interactive interface allows users to explore data insights and make predictions with ease.")
+
+st.header("Overview")
+st.write("Cardiovascular diseases are the leading cause of death globally, making heart attack prediction a critical area of research. Leveraging machine learning, we can gain deeper insights into the factors contributing to heart attacks and develop models that can predict their occurrence with high accuracy.")
+
+st.subheader("Dataset source")
+st.page_link(
+    "https://www.kaggle.com/datasets/rashikrahmanpritom/heart-attack-analysis-prediction-dataset", label="https: // www.kaggle.com/datasets/rashikrahmanpritom/heart-attack-analysis-prediction-dataset")
+
+st.header("Features")
+st.write('''
+### 1. Exploratory Data Analysis (EDA)
+Our application begins with an in-depth exploratory data analysis to understand the underlying patterns and correlations within the dataset. Key features include:
+
+- Visualization of important variables
+- Correlation heatmaps
+- Distribution plots for different health metrics
+- Statistical summaries
+### 2. Predictive Modeling
+We have implemented two powerful predictive models to estimate the risk of heart attacks:
+
+#### Logistic Regression
+**From Scratch**: A custom implementation of logistic regression to illustrate the underlying mechanics.\n
+**Scikit-learn**: Utilizes the widely-used Scikit-learn library for a robust and efficient implementation.
+#### Neural Network
+**From Scratch**: A foundational neural network built from the ground up to demonstrate deep learning principles.\n
+**TensorFlow**: A sophisticated neural network model using TensorFlow, one of the leading deep learning libraries.
+### 3. User-friendly Interface
+Our Streamlit-powered interface ensures that users, regardless of their technical background, can easily interact with the application. Features include:
+
+- Interactive data visualization tools
+- Easy-to-use prediction interfaces
+- Customizable input parameters for personalized risk assessment
+### 4. Model Evaluation
+Comprehensive evaluation metrics to assess the performance of our models, including:
+
+- Accuracy, precision, recall, and F1 score
+- Confusion matrices
+- Classification report''')
